3c4ecf6f57e82035e17dc35e9522e55072bc6d48
Mark Daniel Gutierrez <markdcg@outlook.com>
2024-10-12 17:48:32 +0800

    feat: implement FCFS scheduling, core worker management

---
+++ CoreWorker.cpp
@@ -0,0 +1,131 @@
+#include "CoreWorker.h"
+
+#include <iostream>
+
+CoreWorker::CoreWorker(int id, float delayPerExec, float quantumSlice) 
+    : id(id), delayPerExec(delayPerExec), quantumSlice(quantumSlice), running(false), processAssigned(false) {}
+
+CoreWorker::~CoreWorker() {
+    stop();
+    if (coreThread.joinable()) {
+        coreThread.join();
+    }
+}
+
+std::shared_ptr<Process> CoreWorker::getCurrentProcess() {
+	std::lock_guard<std::mutex> lock(coreMutex);
+	return currentProcess;
+}
+
+void CoreWorker::setProcess(std::shared_ptr<Process> process) {
+    std::lock_guard<std::mutex> lock(coreMutex);
+    currentProcess = process;
+    processAssigned = true;
+}
+
+void CoreWorker::runProcess() {
+    // For FCFS
+    if (quantumSlice == 0) {
+        // While there is still a process, run
+        while (currentProcess && !currentProcess->isFinished()) {
+            totalActiveTicks++;
+            currentProcess->execute();
+            std::this_thread::sleep_for(std::chrono::duration<float>(delayPerExec));
+
+
+        }
+
+        // Process is finished, notify Scheduler
+        if (currentProcess && currentProcess->isFinished()) {
+            finishProcess();
+        }
+    }
+
+    // For RR
+    //else {
+    //    for (int i = 0; i < quantumSlice; i++) {
+    //        if (currentProcess->isFinished()) {
+    //            finishProcess();
+    //            return;
+    //        }
+    //        currentProcess->execute();
+    //        std::this_thread::sleep_for(std::chrono::duration<float>(delayPerExec));
+    //    }
+    //    totalActiveTicks++;
+    //    // Process is not finished, notify Scheduler
+    //    if (processCompletionCallback) {
+    //        processCompletionCallback(currentProcess);
+    //    }
+
+    //    // Reset the current process to ensure it gets requeued
+    //    currentProcess.reset();
+    //    processAssigned = false;
+    //}
+}
+
+
+void CoreWorker::finishProcess() {
+    std::lock_guard<std::mutex> lock(coreMutex);
+    currentProcess.reset(); // Reset the current process
+    processAssigned = false; // No more process assigned
+}
+
+bool CoreWorker::isAvailable() {
+    std::lock_guard<std::mutex> lock(coreMutex);
+    return !processAssigned; // Core is available if no process is assigned
+}
+
+bool CoreWorker::isAssignedProcess() {
+	std::lock_guard<std::mutex> lock(coreMutex);
+	return processAssigned; // Has a process assigned
+}
+
+int CoreWorker::getID() {
+    return id;
+}
+
+void CoreWorker::start() {
+    running = true;
+    coreThread = std::thread(&CoreWorker::run, this); // Start the core thread
+}
+
+void CoreWorker::stop() {
+    running = false;
+    {
+        std::lock_guard<std::mutex> lock(coreMutex);
+        running = false;
+    }
+    if (coreThread.joinable()) {
+        coreThread.join();
+    }
+}
+
+void CoreWorker::run() {
+    // Core thread loop
+    while (true) {
+        {
+            std::lock_guard<std::mutex> lock(coreMutex);
+            if (!running) {
+                break;
+            }
+        }
+        totalCPUTicks++;
+        if (processAssigned) {
+            runProcess();
+        }
+        else {
+            totalIdleTicks++;
+            // TODO: might need to add delay here
+        }
+    }
+}
+
+// Set the process completion callback
+void CoreWorker::setProcessCompletionCallback(std::function<void(std::shared_ptr<Process>)> callback) {
+    std::lock_guard<std::mutex> lock(coreMutex);
+    processCompletionCallback = callback;
+}
+
+std::vector<long long> CoreWorker::getStats() {
+    return { totalCPUTicks, totalActiveTicks, totalIdleTicks };
+}
---
+++ Scheduler.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+#include <queue>
+#include <functional>
+#include <thread>
+#include <mutex>
+#include <windows.h>
+
+#include "Process.h"
+#include "ConfigurationManager.h"
+#include "CoreWorker.h"
+
+class Scheduler
+{
+public:
+    Scheduler();
+    ~Scheduler();
+
+    void addProcess(const Process& process); // Add a process to the ready queue
+    std::shared_ptr<Process> getProcessByName(const std::string name);
+    std::shared_ptr<Process> getProcessByID(int pid);
+
+    bool initialize(ConfigurationManager* newConfigManager); // Initialize the scheduler with a configuration manager
+    void run(); // Start the scheduler
+    void stop();
+    const std::vector<std::unique_ptr<CoreWorker>>& getCores() const; 
+
+    void displayStatus();
+    void saveReport(); 
+
+    // Access to core workers and prcesses
+    const std::vector<std::unique_ptr<CoreWorker>>& getCoreWorkers() const;
+    const std::vector<std::shared_ptr<Process>>& getProcesses() const;
+private:
+    ConfigurationManager* configManager;
+
+    std::vector<std::unique_ptr<CoreWorker>> cores; // All cores
+    std::vector<std::shared_ptr<Process>> processes; // All processes regardless of state
+    std::queue<std::shared_ptr<Process>> readyQueue; // All processes ready to go once a thread yields
+    std::vector<std::shared_ptr<Process>> finishedProcesses; // Add finished processes here
+    std::mutex processMutex; // Protects access to the processes vector
+    std::mutex queueMutex;   // Protects access to the readyQueue
+
+    bool running;
+
+    void initializeCoreWorkers(); // Initialize the cores
+    int getAvailableCoreWorkerID();
+
+    void scheduleFCFS();
+    void scheduleRR(); // TODO: to be implemented
+
+    bool isTestRunning;
+    std::thread     schedulerThread;
+    void schedulerLoop(); // Main loop for the scheduler
+};
