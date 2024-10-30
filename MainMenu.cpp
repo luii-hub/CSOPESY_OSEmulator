cd82ba21248815b00cf75925cb14e4deee619ac4
Luis Rana <migzzzrana@gmail.com>
2024-10-30 23:20:19 +0800

    Revert "Merge pull request #4 from luii-hub/feat/os-improvements"
    This reverts commit b69da8e31e98beb028602e3f52b6c16f7fb18689, reversing
    changes made to 54a3de4d4a8fd1b94a6f79281239b2e922288fe2.

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -42,7 +42,7 @@ void MainMenu::displayHeader(){
 | | |     \___ \| |  | |  ___/|  __|  \___ \  \   /   |
 | | |____ ____) | |__| | |    | |____ ____) |  | |    |
 |  \_____|_____/ \____/|_|    |______|_____/   |_|    |                                                     
- ----------------------------------------------------- 
+ -----------------------------------------------------
     )"))) << endl;
 
     string welcome = "Hello, Welcome to CSOPESY command line!";
@@ -78,7 +78,7 @@ void MainMenu::handleCommand(string command) {
                 trim(processName);
                 if (processName.empty()) { // if the process name is empty
                     cout << "Command not recognized! Please provide a process name." << endl;
-                }
+            }
 
                 // if the process name is not empty
                 else 
@@ -86,7 +86,6 @@ void MainMenu::handleCommand(string command) {
                     // if the process does not exist, create process
                     if (!consoleManager.getResourceManager().processExists(processName)) {
                         consoleManager.createProcessScreen(processName);
-                        consoleManager.switchScreen("PROCESS_SCREEN_" + processName);
                     }
                     else { // if the process already exists
                         cout << "Process already exists or has existed. Please provide a different name." << endl;
@@ -107,30 +106,13 @@ void MainMenu::handleCommand(string command) {
                 }
 
                 else {
-
-                    // Check if the process name exists, else check if the process is finished, else switch screen
-                    if (consoleManager.getResourceManager().processExists(processName)) {
-                        //if it exists, check if the process is finished
-
-                        if (consoleManager.isProcessFinished(processName)) { // if the process is finished, there will be no screen
-                            cout << "Process " << processName << " has already finished. " << endl;
-                        }
-                        else if (!consoleManager.isProcessFinished(processName)) { // if the process is not finished, switch screen
-                        	consoleManager.switchScreen("PROCESS_SCREEN_" + processName); 
-                        }
-
+                    if (consoleManager.isProcessFinished(processName)) { // if the process is finished, there will be no screen
+                        cout << "Process " << processName << " not found." << endl;
                     }
-                    else { // if the process does not exist
-                    	cout << "Process " << processName << " does not exist. " << endl;
+                    else { // if the process is not finished, switch to the screen
+                        consoleManager.switchScreen("PROCESS_SCREEN_" + processName);
                     }
                 }
-            }
-
-            if (command == "screen") {
-                cout << "\nscreen command usage: " << endl;
-                cout << "screen -ls - List all screens" << endl;
-                cout << "screen -s [processName] - Create a new process screen" << endl;
-                cout << "screen -r [processName] - Switch to a process screen" << endl;
             }
 
         } else if (command.substr(0, 9) == "scheduler") {
@@ -154,7 +136,6 @@ void MainMenu::handleCommand(string command) {
 			cout << "clear/cls - Clear the screen" << endl;
 			cout << "screen -ls - List all screens" << endl;
 			cout << "screen -s [processName] - Create a new process screen" << endl;
-            cout << "screen -r [processName] - Switch to a process screen" << endl;
 			cout << "scheduler-test - Start the scheduler test" << endl;
 			cout << "scheduler-stop - Stop the scheduler test" << endl;
 			cout << "report-util - Save the report" << endl;
