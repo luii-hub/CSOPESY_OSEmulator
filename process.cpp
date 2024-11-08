27a6e761551c86964c6728e921d7271b0e323af4
yunjeehaaa <149761887+xEUNicorn@users.noreply.github.com>
2024-11-08 21:26:07 +0800

    Add files via upload

---
+++ process.cpp
@@ -0,0 +1,85 @@
+#include "Process.h"
+#include <iostream>
+
+#include "ConsoleManager.h"
+
+extern ConsoleManager consoleManager;	
+
+Process::Process() : id(0), totalInstructions(0), memorySize(0), pageSize(0), startAddress(0), endAddress(0) {}
+
+Process::Process(std::string name, int id, int totalInstructions, float memorySize, float pageSize)
+    : processName(name), 
+      id(id), 
+      currentInstruction(0), 
+      totalInstructions(totalInstructions), 
+      core(-1), 
+      finished(false), 
+      creationTime(std::chrono::system_clock::now()),
+      memorySize(memorySize),
+      pageSize(pageSize),
+      startAddress(0),
+      endAddress(0)
+{}
+
+// Method to set memory addresses, for later use in Scheduler
+void Process::setMemoryAddresses(int start, int end) {
+    startAddress = start;
+    endAddress = end;
+}
+
+std::string Process::getName() const {
+	return processName;
+}
+
+int Process::getID() const {
+	return id;
+}
+
+int Process::getCurrentInstruction() const {
+	return currentInstruction;
+}
+
+int Process::getTotalInstructions() const {
+	return totalInstructions;
+}
+
+int Process::getRemainingInstructions() const {
+	return totalInstructions - currentInstruction;
+}
+
+int Process::getCore() {
+	return core;
+}
+
+void Process::setCore(int coreID) {
+	core = coreID;
+};
+
+bool Process::isFinished() const {
+	return finished;
+}
+
+std::string Process::getCreationTime() const {
+	auto creationTimeT = std::chrono::system_clock::to_time_t(creationTime);
+	struct tm timeinfo;
+	localtime_s(&timeinfo, &creationTimeT);
+	char buffer[100];
+	std::strftime(buffer, sizeof(buffer), "%m/%d/%Y %I:%M:%S%p", &timeinfo);
+	return buffer;
+}
+
+float Process::getMemorySize() const {
+	return memorySize;
+}
+
+float Process::getPageSize() const {
+	return pageSize;
+}
+
+void Process::execute() {
+	if (currentInstruction < totalInstructions) { // If the process has not finished executing
+		currentInstruction++; // Increment the current instruction
+	} else {
+		finished = true; // Set the process to finished
+	}
+}
---
+++ process.h
@@ -0,0 +1,43 @@
+// Process.h
+
+class Process {
+private:
+    int id;
+    std::string processName;
+    int currentInstruction;
+    int totalInstructions;
+    int core;
+    bool finished;
+    float memorySize;
+    float pageSize;
+    int startAddress;   // New member for start address
+    int endAddress;     // New member for end address
+    std::chrono::time_point<std::chrono::system_clock> creationTime;
+
+public:
+    Process();
+    Process(std::string name, int id, int totalInstructions, float memorySize, float pageSize);
+
+    std::string getName() const;
+    int getID() const;
+    int getCurrentInstruction() const;
+    int getTotalInstructions() const;
+    int getRemainingInstructions() const;
+    int getCore();
+    void setCore(int coreID);
+    bool isFinished() const;
+    std::string getCreationTime() const;
+    float getMemorySize() const;
+    float getPageSize() const;
+    void execute();
+
+    // New getter methods for memory addresses
+    int getStartAddress() const { return startAddress; }
+    int getEndAddress() const { return endAddress; }
+    
+    // New method to set memory addresses
+    void setMemoryAddresses(int start, int end) {
+        startAddress = start;
+        endAddress = end;
+    }
+};
