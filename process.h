42a4708623e6b3c1a88e1a1f47dd14db3efc0141
yunjeehaaa <149761887+xEUNicorn@users.noreply.github.com>
2024-11-08 22:23:18 +0800

    Update process.h

--- process.h
+++ process.h
@@ -2,44 +2,38 @@
 #include <chrono>
 #include <string>
 
-class Process {
-private:
-    int id;
-    std::string processName;
-    int currentInstruction;
-    int totalInstructions;
-    int core;
-    bool finished;
-    float memorySize;
-    float pageSize;
-    int startAddress;   // New member for start address
-    int endAddress;     // New member for end address
-    std::chrono::time_point<std::chrono::system_clock> creationTime;
-
+class Process
+{
 public:
-    Process();
-    Process(std::string name, int id, int totalInstructions, float memorySize, float pageSize);
+	Process();
+	Process(std::string name, int id, int totalInstructions, float memorySize, float pageSize);
 
-    std::string getName() const;
-    int getID() const;
-    int getCurrentInstruction() const;
-    int getTotalInstructions() const;
-    int getRemainingInstructions() const;
-    int getCore();
-    void setCore(int coreID);
-    bool isFinished() const;
-    std::string getCreationTime() const;
-    float getMemorySize() const;
-    float getPageSize() const;
-    void execute();
+	std::string getName() const;
+	int getID() const;
+	int getCurrentInstruction() const;
+	int getTotalInstructions() const;
+	int getRemainingInstructions() const;
+	int getCore();
+	void setCore(int coreID);
+	bool isFinished() const;
+	std::string getCreationTime() const;
+	float getMemorySize() const;
+	float getPageSize() const;
 
-    // New getter methods for memory addresses
-    int getStartAddress() const { return startAddress; }
-    int getEndAddress() const { return endAddress; }
-    
-    // New method to set memory addresses
-    void setMemoryAddresses(int start, int end) {
-        startAddress = start;
-        endAddress = end;
-    }
+	void execute(); // Executes one instruction
+
+private:
+	std::string processName;
+	int id;
+	int core;
+	std::string status;
+	std::chrono::system_clock::time_point creationTime;
+
+	bool finished;
+	int currentInstruction;
+	int totalInstructions;
+	
+	float memorySize;
+	float pageSize;
 };
+
